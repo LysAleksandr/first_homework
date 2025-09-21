9764b47b84fb9d3775df618cb8e1fa0b59a2474b
LysAleksandr <cogi_l@mail.ru>
2025-09-21 21:09:18 +0300

    Add MainClass.java with THING

---
+++ src/main/java/com/mipt/lysaleksandr/MainClass.java
@@ -0,0 +1,12 @@
+package com.mipt.lysaleksandr;
+
+public class MainClass {
+  private Integer thingInt;
+  private String thingString;
+  protected static double thingDouble;
+  public final long thingLong = 69;
+  public static void main(String[] args) {
+
+  }
+
+}
