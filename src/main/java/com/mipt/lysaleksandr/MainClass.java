513bfce95fb669d97a7d98a9ebb88c69fc1f200d
LysAleksandr <cogi_l@mail.ru>
2025-09-21 21:12:09 +0300

    Add cycle from 0 to 15, which print ind

--- src/main/java/com/mipt/lysaleksandr/MainClass.java
+++ src/main/java/com/mipt/lysaleksandr/MainClass.java
@@ -6,7 +6,9 @@ public class MainClass {
   protected static double thingDouble;
   public final long thingLong = 69;
   public static void main(String[] args) {
-
+    for (int ind = 0; ind < 15; ind++) {
+      System.out.println("Iter: " + ind);
+    }
   }
 
 }
