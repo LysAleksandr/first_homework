4bfa33907b96002a6752fc2b75c0de64a5313cf5
LysAleksandr <cogi_l@mail.ru>
2025-09-21 20:55:26 +0300

    Add package model with java class describing a person

---
+++ src/main/java/com/mipt/lysaleksandr/model/Human.java
@@ -0,0 +1,40 @@
+package com.mipt.lysaleksandr.model;
+
+public class Human {
+  private String name;
+  private String surname;
+  private Integer age;
+  private boolean work;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge() {
+    this.age = age;
+  }
+
+  public boolean getWork() {
+    return work;
+  }
+
+  public void setWork() {
+    this.work = work;
+  }
+}
