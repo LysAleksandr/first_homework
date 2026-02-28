dd6bf78da51cc408267d347a3da54f9a0bb327b5
LysAleksandr <cogi_l@mail.ru>
2026-02-28 22:34:26 +0300

    Completed the second part of the task

--- src/main/java/com/mipt/lysaleksandr/todomanager/TodoManagerApplication.java
+++ src/main/java/com/mipt/lysaleksandr/todomanager/TodoManagerApplication.java
@@ -4,13 +4,14 @@ import com.mipt.lysaleksandr.todomanager.service.TaskStatisticsService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class TodoManagerApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(TodoManagerApplication.class,
-            args);
+        ConfigurableApplicationContext context = SpringApplication.run(TodoManagerApplication.class, args);
 
         TaskStatisticsService statisticsService = context.getBean(TaskStatisticsService.class);
         statisticsService.printStatistics();
