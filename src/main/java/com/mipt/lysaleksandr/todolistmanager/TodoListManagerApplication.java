e417f240a6b0c51cc8cf57b4d6295c24774126ca
LysAleksandr <cogi_l@mail.ru>
2026-03-15 22:32:40 +0300

    Completed the second part of the homework

--- src/main/java/com/mipt/lysaleksandr/todolistmanager/TodoListManagerApplication.java
+++ src/main/java/com/mipt/lysaleksandr/todolistmanager/TodoListManagerApplication.java
@@ -4,8 +4,10 @@ import com.mipt.lysaleksandr.todolistmanager.service.TaskStatisticsService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class TodoListManagerApplication {
 
     public static void main(String[] args) {
